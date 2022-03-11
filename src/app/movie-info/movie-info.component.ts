6608ed11771b2ab69d28c73c31ffdbf438d16ce0
josemavit24 <josemavit24@gmail.com>
2022-03-11 15:56:24 -0500

    Iterates through API

--- src/app/movie-info/movie-info.component.ts
+++ src/app/movie-info/movie-info.component.ts
@@ -11,23 +11,36 @@ import { MovieInfo } from './movie-info.model';
 export class MovieInfoComponent implements OnInit {
 
   movInfo: MovieInfo | undefined;
+
+  movieList: MovieInfo[] = [];
   
+
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     console.log("Sending Request to Server");
-    this.showMovieInfo();
+    for (let movie_id = 2; movie_id < 15000; movie_id++) {
+      this.showMovieInfo(movie_id);
+      
+    }
+    
   }
 
-  getMovieInfo(){
-    return this.http.get<MovieInfo>('https://api.themoviedb.org/3/movie/550?api_key=ba25ba134879219e9e3c39e8aeb9d179');
+  getMovieInfo(movie_id: number){
+    return this.http.get<MovieInfo>('https://api.themoviedb.org/3/movie/'+ movie_id +'?api_key=ba25ba134879219e9e3c39e8aeb9d179');
   }
 
-  showMovieInfo(){
-    this.getMovieInfo().subscribe((data: MovieInfo)=>{
+  showMovieInfo(movie_id: number){
+    this.getMovieInfo(movie_id).subscribe((data: MovieInfo)=>{
       console.log(data);
       this.movInfo = data;
+      if (this.movInfo == undefined){
+        return;
+      }
+      else{
+        this.movieList.push(data)
+      }
     })
   }
 }
