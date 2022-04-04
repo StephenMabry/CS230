a2284baab4a584a6eaf2ad448658c97b368db2d1
StephenMabry <sm00010@mix.wvu.edu>
2022-04-04 15:46:09 -0400

    Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MovieInfoComponent } from './movie-info/movie-info.component';
 import { HomeLayoutComponent } from './home-layout/home-layout.component';
+import { TopNavbarComponent } from './top-navbar/top-navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MovieInfoComponent,
-    HomeLayoutComponent
+    HomeLayoutComponent,
+    TopNavbarComponent
   ],
   imports: [
     BrowserModule,
