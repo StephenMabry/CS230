7ae2717da6b90fe8c4f171c0b6d4c0fa145db407
David Clark <djc00001@mix.wvu.edu>
2022-04-06 11:55:23 -0400

    Created User Profile page, set up routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserProfileComponent } from './user-profile-page/user-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'User-Profile', component: UserProfileComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
