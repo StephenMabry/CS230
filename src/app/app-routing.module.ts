492ab3a50bf68ce57eb335b76b40071e6e0cc304
Jack Amling <jaa0054@mix.wvu.edu>
2022-04-08 14:40:28 -0400

    created path for home layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './Users/user.component';
 import { UserProfileComponent } from './user-profile-page/user-profile.component';
+import { HomeLayoutComponent } from './home-layout/home-layout.component';
 
 const routes: Routes = [
+  {path: '', component: HomeLayoutComponent},
   {path: 'auth', component: UserComponent},
   {path: 'User-Profile', component: UserProfileComponent},
 ];
