6f585b2bb728c45bccbbae827a95fc96ab1a3f38
David Clark <djc00001@mix.wvu.edu>
2022-04-11 11:46:22 -0400

    Created preferred, disfavored tabs

--- src/app/Interests-Page/button-selections.component.ts
+++ src/app/Interests-Page/button-selections.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector: 'user-profile',
+    selector: 'button-selections',
     templateUrl: 'button-selections.component.html',
     styleUrls: ['button-selections.component.css']
   })
