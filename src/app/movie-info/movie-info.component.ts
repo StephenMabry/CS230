8917051c711d8375245f87d96a87d4a3df867534
shivan <68702232+shivanprasad@users.noreply.github.com>
2022-04-20 22:33:27 -0400

    feat: limits movie-info.component.html to only display 5 movies (adjustable)

--- src/app/movie-info/movie-info.component.ts
+++ src/app/movie-info/movie-info.component.ts
@@ -23,9 +23,7 @@ export class MovieInfoComponent implements OnInit {
     console.log("Sending Request to Server");
     for (let movie_id = 2; movie_id < 2000; movie_id++) {
       this.showMovieInfo(movie_id);
-
     }
-
   }
 
   getMovieInfo(movie_id: number){
