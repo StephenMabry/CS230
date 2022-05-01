7bc144f6dd65a08433b8d6b035e600222f9aa5ff
josemavit24 <josemavit24@gmail.com>
2022-05-01 13:48:56 -0400

    Fixed routing to movie details

---
+++ src/app/movie-page/movie-page-similar.model.ts
@@ -0,0 +1,25 @@
+export interface SimilarMovies{
+        page: number,
+        results: [
+          {
+            adult: boolean,
+            backdrop_path: string,
+            genre_ids: [
+              number
+            ],
+            id: number,
+            original_language: string,
+            original_title: "Darna! Ang Pagbabalik",
+            overview: "Valentina, Darna's snake-haired arch enemy, is trying to take over the Phillipines through subliminal messages on religious TV shows. Darna has her own problems, however, as she has lost her magic pearl and with it the ability to transform into her scantily clad super self. Trapped as her alter-ego, the plucky reporter Narda, she must try to regain the pearl and foil Valentina's plans.",
+            release_date: "1994-05-09",
+            poster_path: null,
+            popularity: 1.012564,
+            title: "Darna: The Return",
+            video: false,
+            vote_average: 0,
+            vote_count: 0
+          }
+        ],
+        total_pages: 9,
+        total_results: 168
+}
--- src/app/movie-page/movie-page.component.ts
+++ src/app/movie-page/movie-page.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MovieInfoComponent} from '../movie-info/movie-info.component';
-import {MovieInfo} from '../movie-info/movie-info.model'
+import { MovieInfo} from '../movie-info/movie-info.model'
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'm2d-movie-page',
@@ -10,18 +11,29 @@ import {MovieInfo} from '../movie-info/movie-info.model'
 })
 export class MoviePageComponent implements OnInit {
 
-  movie: MovieInfo | undefined;
-  movie_poster: string = "https://image.tmdb.org/t/p/w600_and_h900_bestv2/";
 
-  constructor(private route:ActivatedRoute, private mov: MovieInfoComponent) { }
+  movie_poster: string = "https://image.tmdb.org/t/p/w600_and_h900_bestv2/";
+  public id: string| null | undefined;
+  public mov: MovieInfo | undefined;
+  constructor(private route: ActivatedRoute, private http: HttpClient) {
+    this.route.params.subscribe(params => console.log(params));
+
+  }
 
   ngOnInit(): void {
-    const routeParams = this.route.snapshot.paramMap;
-    const movieIdFromRoute = Number(routeParams.get('id'));
-    this.mov.getMovieInfo(movieIdFromRoute).subscribe((data: MovieInfo)=>{
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.showMovieInfo(Number(this.id));
+  }
+
+  getMovieInfo(movie_id: number){
+    return this.http.get<MovieInfo>('https://api.themoviedb.org/3/movie/'+ movie_id +'?api_key=ba25ba134879219e9e3c39e8aeb9d179');
+  }
+
+  showMovieInfo(movie_id: number){
+    this.getMovieInfo(movie_id).subscribe((data: MovieInfo)=>{
       console.log(data);
-      this.movie = data;
+      this.mov = data;
     })
-  } 
+  }
 
 }
