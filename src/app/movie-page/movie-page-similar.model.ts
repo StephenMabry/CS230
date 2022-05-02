e5a7009ad45f84f1730c9e7839716da94b8f8a14
josemavit24 <josemavit24@gmail.com>
2022-05-02 01:00:24 -0400

    Added more details about movie and similar movies

--- src/app/movie-page/movie-page-similar.model.ts
+++ src/app/movie-page/movie-page-similar.model.ts
@@ -9,17 +9,17 @@ export interface SimilarMovies{
             ],
             id: number,
             original_language: string,
-            original_title: "Darna! Ang Pagbabalik",
-            overview: "Valentina, Darna's snake-haired arch enemy, is trying to take over the Phillipines through subliminal messages on religious TV shows. Darna has her own problems, however, as she has lost her magic pearl and with it the ability to transform into her scantily clad super self. Trapped as her alter-ego, the plucky reporter Narda, she must try to regain the pearl and foil Valentina's plans.",
-            release_date: "1994-05-09",
-            poster_path: null,
-            popularity: 1.012564,
-            title: "Darna: The Return",
+            original_title: string,
+            overview: string,
+            release_date: string,
+            poster_path: string | null,
+            popularity: number,
+            title: string,
             video: false,
-            vote_average: 0,
-            vote_count: 0
+            vote_average: number,
+            vote_count: number
           }
         ],
-        total_pages: 9,
-        total_results: 168
+        total_pages: number,
+        total_results: number
 }
--- src/app/movie-page/movie-page.component.ts
+++ src/app/movie-page/movie-page.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { MovieInfoComponent} from '../movie-info/movie-info.component';
+import { ActivatedRoute, Router } from '@angular/router';
 import { MovieInfo} from '../movie-info/movie-info.model'
 import { HttpClient } from '@angular/common/http';
+import { SimilarMovies } from './movie-page-similar.model';
 
 @Component({
   selector: 'm2d-movie-page',
@@ -12,21 +12,29 @@ import { HttpClient } from '@angular/common/http';
 export class MoviePageComponent implements OnInit {
 
 
-  movie_poster: string = "https://image.tmdb.org/t/p/w600_and_h900_bestv2/";
+  movie_poster: string = "https://image.tmdb.org/t/p/original/";
   public id: string| null | undefined;
   public mov: MovieInfo | undefined;
-  constructor(private route: ActivatedRoute, private http: HttpClient) {
+  public recommendationsList: SimilarMovies | undefined;
+  constructor(private route: ActivatedRoute, private http: HttpClient, private router: Router) {
     this.route.params.subscribe(params => console.log(params));
+
 
   }
 
   ngOnInit(): void {
+    
     this.id = this.route.snapshot.paramMap.get('id');
     this.showMovieInfo(Number(this.id));
+    this.showSimilarMovies(Number(this.id));
   }
 
   getMovieInfo(movie_id: number){
     return this.http.get<MovieInfo>('https://api.themoviedb.org/3/movie/'+ movie_id +'?api_key=ba25ba134879219e9e3c39e8aeb9d179');
+  }
+
+  getSimilarMovies(movie_id: number){
+    return this.http.get<SimilarMovies>('https://api.themoviedb.org/3/movie/'+movie_id+'/similar?api_key=ba25ba134879219e9e3c39e8aeb9d179&page=1')
   }
 
   showMovieInfo(movie_id: number){
@@ -34,6 +42,17 @@ export class MoviePageComponent implements OnInit {
       console.log(data);
       this.mov = data;
     })
+  }
+
+  showSimilarMovies(movie_id: number){
+    this.getSimilarMovies(movie_id).subscribe((data: SimilarMovies)=>{
+      console.log(data);
+      this.recommendationsList = data;
+    })
+  }
+
+  refresh(){
+    this.ngOnInit();
   }
 
 }
